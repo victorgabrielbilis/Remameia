7bcb9153c80379d6bc9be748912ec2072817bf87
MatheusPissaia <matpissaia@gmail.com>
2018-09-18 23:16:37 -0300

    Apaguei informação no footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -218,15 +218,4 @@
           </div>
         </div>
       </div>
-      <!-- GET IT-->
-      <div class="get-it">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 text-center p-3">
-              <h3>Do you want cool website like this one?</h3>
-            </div>
-            <div class="col-lg-4 text-center p-3">   <a href="#" class="btn btn-template-outlined-white">Buy this template now</a></div>
-          </div>
-        </div>
-      </div>
 @endsection
