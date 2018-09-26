5cb9da3951629cac6029258752bb9d64c1ac30a0
Matheus Pissaia <matpissaia@gmail.com>
2018-09-26 00:18:48 -0300

    Deliver

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,34 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="pt-br" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
     @include('layouts.head')
+
+    <style type="text/css">
+      .linked-in{
+     background-color:#007bb6;
+      }
+     .facebook{
+         background-color:#3b5998;
+     }
+     .twitter{
+         background-color:#1da1f2;
+     }
+     .google{
+         background-color:#f63e28;
+      }
+     .social-icon {
+      width: 30px;
+      height: 30px;
+      font-size: 15px;
+      color: #fff;
+      text-align: center;
+      margin-right: 10px;
+      padding-top: 7px;
+      border-radius: 50%;
+      }
+    </style>
   </head>
   <body>
       @include('layouts.bar')
